cecd8cadae1af52234bdb766b53255d79ed6931f
nicius2 <marco.marks06@gmail.com>
2023-08-15 00:18:26 -0300

    javaScript-arrays

---
+++ media.js
@@ -0,0 +1,21 @@
+// Criar uma lista com medias de alunos 
+// NOTAS DOS ALUNOS: 10, 6.5, 8, 7.5;
+
+// crinado com variaveis
+//const nota1 = 10;
+//const nota2 = 6.5;
+//const nota3 = 8;
+//const nota4 = 7.5;
+
+//console.log((nota1 + nota2 + nota3 + nota4) / 4)
+
+// criando com uma lista
+
+// POSIÇÕES:
+//              0   1   2   3
+const notas = [10, 6.5, 8, 7.5]
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media)
+
