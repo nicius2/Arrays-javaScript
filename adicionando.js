669b7d4cf921315574da5818d47b7bc7f629c097
nicius2 <marco.marks06@gmail.com>
2023-08-17 13:21:44 -0300

    arrays

---
+++ adicionando.js
@@ -0,0 +1,12 @@
+const notas = [10, 9, 8];
+
+// Se quiser adicionar mais uma valor nesse array (LISTA)
+// deve atribuir o valor <push>
+
+notas.push(7)
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media)
+
+console.log(notas);
---
+++ removendo-notas.js
@@ -0,0 +1,6 @@
+const notas = [10, 6, 8, 5.5, 10];
+
+// <pop> é usado para remover um valor da array
+notas.pop();
+
+console.log(`minha nota é ${notas}.`)
