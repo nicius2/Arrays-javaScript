8b9008e216448625b1505945f5e60af51986fe9e
nicius2 <marco.marks06@gmail.com>
2023-08-18 11:48:52 -0300

    arrays

---
+++ atualizando-lista.js
@@ -0,0 +1,7 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 2, "rodrigo");
+
+//Poderia também ser feito com o (nome.push) para adicionar a string "rodrigo"
+
+console.log(nomes)
