1d7ce3dc593cbc4e7f8788813f3d841de66896e1
nicius2 <marco.marks06@gmail.com>
2023-08-18 11:30:20 -0300

    arrays

---
+++ dividindo-sala.js
@@ -0,0 +1,34 @@
+// slice() = é usado para dividir as arrays
+
+const alunos = [
+    "João",
+    "Juliana",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Aline",
+    "Fabiana",
+    "Andre",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isabela",
+    "Vinícius",
+    "Renan",
+    "Renata",
+    "Daisy",
+    "Camilo",
+];
+
+//const sala1 = alunos.slice(0,10);
+//const sala2 = alunos.slice(10);
+
+// CODIGO ACIMA MELHORADO
+const sala1 = alunos.slice(0, alunos.length / 2);
+const sala2 = alunos.slice(alunos.length / 2);
+
+console.log(sala1)
+console.log(sala2)
